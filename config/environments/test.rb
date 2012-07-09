c2eb3ed4438cf0a18e920f56847c62782aace8ed
Jason Dolloff <penicore@gmail.com>
2012-07-09 18:51:56 -0500

    Finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Reduce test time with BCrypt, reduce its cost function
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
